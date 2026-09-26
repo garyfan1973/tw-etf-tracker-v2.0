38f5a461c716c469c35d1c02a13bfa86aa037777
Gary Fan <garyfan1973@gmail.com>
2026-09-26 20:29:29 +0800

    feat: add rose morandi palette

--- webapp/theme.js
+++ webapp/theme.js
@@ -2,7 +2,7 @@
 (function () {
   const KEY = "etf-theme";
   const PALETTE_KEY = "etf-palette";
-  const PALETTES = ["mist", "sage", "lilac", "sand"];
+  const PALETTES = ["mist", "sage", "lilac", "sand", "rose"];
   const root = document.documentElement;
 
   function system() {
